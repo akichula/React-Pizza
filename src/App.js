3685a27b20f028273d3daccf032d950830515abc
akichula <masterfrag111@gmail.com>
2020-12-26 20:49:49 +0400

    PizzaBlock component with smart-props

--- src/App.js
+++ src/App.js
@@ -8,12 +8,20 @@ import Main from "./pages/Main";
 import Cart from "./pages/Cart";
 
 const App = () => {
+    const [pizzas, setPizzas] = React.useState([]);
+
+    React.useEffect(() => {
+        fetch('http://localhost:3000/db.json')
+            .then((response) => response.json())
+            .then((json) => setPizzas(json.pizzas));
+    }, []);
+
     return (
         <div className="wrapper">
             <Header/>
             <div className='content'>
-            <Route exact path={'/'} component={Main}/>
-            <Route exact path={'/cart'} component={Cart}/>
+                <Route exact path={'/'} render={() => <Main items={pizzas}/>}/>
+                <Route exact path={'/cart'} component={Cart}/>
             </div>
         </div>
     )
