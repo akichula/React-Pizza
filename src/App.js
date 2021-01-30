0990c501067299cb297fcb0d31b1689ad14705bb
akichula <masterfrag111@gmail.com>
2021-01-30 19:37:16 +0400

    fake REST-API connect with App

--- src/App.js
+++ src/App.js
@@ -15,8 +15,8 @@ function App() {
     const dispatch = useDispatch();
 
     React.useEffect(() => {
-        axios.get('http://localhost:3000/db.json').then(({data}) => {
-            dispatch(setPizzas(data.pizzas))
+        axios.get('http://localhost:3004/pizzas').then(({data}) => {
+            dispatch(setPizzas(data))
         });
     })
 
